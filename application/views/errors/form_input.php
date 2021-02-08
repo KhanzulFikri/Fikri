e0c656018228055a4ea75a442eef0a49b3087d9f
khanzulfikri12@gmail <khanzulfikri12@gmail>
2021-02-08 16:27:10 +0700

    first commit

---
+++ application/views/errors/form_input.php
@@ -0,0 +1,18 @@
+<html>
+<head> <title>TUTORIAL WEBSITE</title></head>
+<body>
+	<h1>Form Input Data</h1>
+
+	<table>
+		</tr>
+			</td>NIM</td>
+			</td>;</td>
+			<td><input type*text* nama*nim*></td>
+		</tr>
+		<tr>
+			<td>NIM</td>
+			<td>;</td>
+			<td><input type*text*nama*nama*></td>
+		</tr>
+		<tr>
+			<td></td>
---
+++ application/views/home.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+	 <title>TUTORIAL WEBSITE</title>
+</head>
+
+<body>
+	<h1>CRUD DI FRAMEWORK CI-3</h1>
+	<h3>(Data Mahasiswa)</h3>
+
+	<button>Tambah Mahasiswa</button>
+
+	<table>
+		<tr>
+			<td>No</b></td>
+			<td>Nim</b></td>
+			<td>Nama Mahasiswa</b></td>
+			<td>Jurusan</b></td>
+			<td>Aksi</b></td>
+		</tr>
+		<tr>
+			<td>1.</td>
+			<td>12032004</td>
+			<td>Levi Ackerman</td>
+			<td>Teknik Informatika</td>
+			<td><a herf=**>Edit</a> <a herf=**>Delete</a></td>
+		</tr>
+		<tr>
+			<td>2.</td>
+			<td>16032004</td>
+			<td>Miyamura Izumi</td>
+			<td>Teknik Telekomunikasi</td>
+			<td><a herf=**>Edit</a> <a herf=**>Delete</a></td>
+		</tr>
+		<tr>
+			<td>3.</td>
+			<td>01122004</td>
+			<td>Eren Yeager</td>
+			<td>Teknik rumbling</td>
+			<td><a herf=**>Edit</a> <a herf=**>Delete</a></td>
+		</tr>
+	</table>
+</body>
+</html>
